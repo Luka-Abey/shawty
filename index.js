c081b78ae2b8868d25c85bf8e46f7e6673d7ccf1
Luka Abeywickrama <luka.abeywickrama@hotmail.co.uk>
2020-11-22 19:31:45 +0000

    changed server ports etc

--- index.js
+++ index.js
@@ -96,7 +96,7 @@ async (req, res, next) => {
 //   });
 // });
 
-const port = process.env.PORT || 1337;
-app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}`);
+var server = app.listen(process.env.PORT || 5000, function () {
+  var port = server.address().port;
+  console.log("Express is working on port " + port);
 });
